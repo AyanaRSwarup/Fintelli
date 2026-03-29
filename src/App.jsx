e4982d1fb0d69c9d582cf4dec8fbfb048f2d8b89
Tijil Sharma <sharmatijil06@gmail.com>
2026-03-30 00:58:03 +0530

    Feat: Added Onboarding Form, along with error mitigation and connection to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Signup from './pages/Signup'
 import { Routes, Route } from "react-router-dom"
+import Onboarding from './pages/Onboarding.jsx'
+import FourZeroFour from './pages/Error.jsx'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Signup />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/error" element={<FourZeroFour />} />
       </Routes>
     </div>
   )
