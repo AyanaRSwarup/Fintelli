58dbb3341e94c6e71f0c808af404540b6516ee50
Tijil Sharma <sharmatijil06@gmail.com>
2026-03-30 18:46:59 +0530

    bug: protected the route from unauthorized access

--- src/context/ProfileContext.jsx
+++ src/context/ProfileContext.jsx
@@ -1,46 +1,64 @@
-import { useContext, createContext, useState, useEffect } from "react";
+import { useContext, createContext, useState, useEffect} from "react";
+import { Navigate } from "react-router-dom";
 
 export const ProfileContext = createContext(null);
 
-
 export function ProfileProvider({ children }) {
-    const [profile, setProfile] = useState({
-        name: "",
-        email: "",
-        profilePicture: ""
-    });
+  const [profile, setProfile] = useState(null); 
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        async function fetchProfile(){
-            const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/getProfile`, {
-                method: "GET",
-                credentials: "include"
-            });
-            if(res.ok){
-                const data = await res.json();
-                setProfile({
-                    name: data.name,
-                    email: data.email,
-                    profilePicture: data.profilePicture
-                });
-            } else {
-                setProfile({
-                    name: "",
-                    email: "",
-                    profilePicture: ""
-                });
-            }
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/api/auth/getProfile`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
+
+        if (res.ok) {
+          const data = await res.json();
+          setProfile({
+            name: data.name,
+            email: data.email,
+            profilePicture: data.picture,
+          });
+        } else {
+          setProfile(null);
         }
+      } catch (error) {
+        setProfile(null);
+      } finally {
+        setLoading(false);
+      }
+    }
 
-        fetchProfile();
-    }, [])
-    return(
-        <ProfileContext.Provider value={{ profile, setProfile }}>
-            {children}
-        </ProfileContext.Provider>
-    )
+    fetchProfile();
+  }, []);
+
+  return (
+    <ProfileContext.Provider value={{ profile, setProfile, loading }}>
+      {children}
+    </ProfileContext.Provider>
+  );
 }
 
-export function useProfile(){
-    return useContext(ProfileContext);
+export function useProfile() {
+  return useContext(ProfileContext);
+}
+
+export default function ProtectedRoute({ children }) {
+  const { profile, loading } = useProfile();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!profile) {  
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
 }
